da54d49773a5dd00fd36df86ca755a36f543a7ce
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-02-07 00:54:52 -0600

    auto-fix puppet-lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,22 +3,21 @@
 # This class is called from realmd for install.
 #
 class realmd::install {
-
-  package { $::realmd::realmd_package_name:
-    ensure => $::realmd::realmd_package_ensure,
+  package { $realmd::realmd_package_name:
+    ensure => $realmd::realmd_package_ensure,
   }
 
-  package { $::realmd::adcli_package_name:
-    ensure => $::realmd::adcli_package_ensure,
+  package { $realmd::adcli_package_name:
+    ensure => $realmd::adcli_package_ensure,
   }
 
-  package { $::realmd::krb_client_package_name:
-    ensure => $::realmd::krb_client_package_ensure,
+  package { $realmd::krb_client_package_name:
+    ensure => $realmd::krb_client_package_ensure,
   }
 
   if $realmd::manage_sssd_package {
-    package { $::realmd::sssd_package_name:
-      ensure => $::realmd::sssd_package_ensure,
+    package { $realmd::sssd_package_name:
+      ensure => $realmd::sssd_package_ensure,
     }
   }
 
