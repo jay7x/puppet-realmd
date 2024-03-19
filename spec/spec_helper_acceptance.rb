4567d54e73c36f7a827b2d5b35365bd59c631f51
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2024-03-19 09:30:33 -0500

    basic litmus acceptance testing

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,25 +1,6 @@
-require 'beaker-rspec/spec_helper'
-require 'beaker-rspec/helpers/serverspec'
-require 'beaker-puppet'
-include BeakerPuppet
+# frozen_string_literal: true
 
-RSpec.configure do |c|
-  # Project root
-  proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+require 'puppet_litmus'
+PuppetLitmus.configure!
 
-  # Readable test descriptions
-  c.formatter = :documentation
-
-  # Configure all nodes in nodeset
-  c.before :suite do
-    install_puppet_on(hosts)
-
-    # Install module and dependencies
-    hosts.each { |h| h[:distmoduledir] = get_target_module_path(h) }
-    mod_name = parse_for_modulename(proj_root).last
-    install_dev_puppet_module_on(hosts, source: proj_root, module_name: mod_name)
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { acceptable_exit_codes: [0, 1] }
-    end
-  end
-end
+require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
